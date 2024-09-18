2d27f8547862e7bb9b98638246fdceb7ff3bf316
nhapham <nha.pham@ikemso.com>
2024-09-19 00:20:19 +0700

    fix: pop youtube player

--- packages/youtube_player_flutter/lib/src/widgets/youtube_player_builder.dart
+++ packages/youtube_player_flutter/lib/src/widgets/youtube_player_builder.dart
@@ -31,8 +31,7 @@ class YoutubePlayerBuilder extends StatefulWidget {
   State<YoutubePlayerBuilder> createState() => _YoutubePlayerBuilderState();
 }
 
-class _YoutubePlayerBuilderState extends State<YoutubePlayerBuilder>
-    with WidgetsBindingObserver {
+class _YoutubePlayerBuilderState extends State<YoutubePlayerBuilder> with WidgetsBindingObserver {
   final GlobalKey playerKey = GlobalKey();
 
   @override
@@ -74,7 +73,9 @@ class _YoutubePlayerBuilderState extends State<YoutubePlayerBuilder>
           if (controller.value.isFullScreen) {
             widget.player.controller.toggleFullScreenMode();
           } else {
-            Navigator.pop(context);
+            if (!didPop) {
+              Navigator.pop(context);
+            }
           }
         },
         child: widget.player,
